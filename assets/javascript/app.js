25e63a276970d33e8def2d053306e131c167e31a
morgansliman <morgansliman@gmail.com>
2016-10-31 17:40:19 -0400

    started writing logic to start the game based on mode chosen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,4 +3,41 @@ $(document).ready(function() {
 		$(this).hide();
 		$('.mode-list').show();
 	});
+
+	$('.button-mode').on('click', function() {
+		$('.mode-list').hide();
+		newGame = new Game($(this).text());
+		newGame.startGame();
+	});
+
+	function Game (mode) {
+		this.mode = mode;
+	}
+
+	Game.prototype.startGame = function() {
+		var div = $('<div>', {
+			'class': 'center-block',
+			'data-mode': this.mode.toLowerCase()
+		}).html(
+		'<p class="time-remaining-text">Time Remaining: <span id="time-remaining">' + 30 +
+		'</span> Seconds</p><br>' + 
+		'<p class="question-text">placeholder</p><br>' +
+		'<ul class="question-list">' +
+		'<li class="question">' + 'question 1 placeholder' + '</li>' +
+		'<li class="question">' + 'question 2 placeholder' + '</li>' +
+		'<li class="question">' + 'question 3 placeholder' + '</li>' +
+		'<li class="question">' + 'question 4 placeholder' + '</li>' +
+		'</ul>'
+		);
+/*
+		var p_time = $('<p>', {
+			'class': 'time-remaining-text'
+		}).text('Time Remaining: <span id="time-remaining">' + 30 + '</span> Seconds');
+
+		var p_question = $('<p>', {
+			'class': 'question-text'
+		}).text('placeholder');
+*/
+		$('.main-game').append(div);
+	}
 });
