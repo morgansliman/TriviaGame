14e72e51e5cb65353d59ef420271245bddb2f2aa
morgansliman <morgansliman@gmail.com>
2016-11-01 02:09:11 -0400

    added placeholder questions & answers with working countdown

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,38 +6,68 @@ $(document).ready(function() {
 
 	$('.button-mode').on('click', function() {
 		$('.mode-list').hide();
-		newGame = new Game($(this).text());
-		newGame.startGame();
+		game.startGame($(this).text());
 	});
 
-	function Game (mode) {
-		this.mode = mode;
+	var game = {
+		timer: 10,
+
+		startGame: function(mode) {
+			var div = $('<div>', {
+				'class': 'center-block game-wrapper',
+				'data-mode': mode.toLowerCase()
+			}).html(
+			'<p class="time-remaining-text">Time Remaining: <span id="time-remaining">' + 
+			game.timer + ' Seconds</span></p>' + 
+			'<p class="question-text">placeholder</p><br>' +
+			'<ul class="question-list center-block">' +
+			'<li class="question">' + 'question 1 placeholder' + '</li>' +
+			'<li class="question">' + 'question 2 placeholder' + '</li>' +
+			'<li class="question">' + 'question 3 placeholder' + '</li>' +
+			'<li class="question">' + 'question 4 placeholder' + '</li>' +
+			'</ul>'
+			);
+
+			$('.main-game').append(div);
+			this.startCountdown();
+		},
+
+		startCountdown: function() {
+			counter = setInterval(game.countdown, 1000);
+		},
+
+		countdown: function() {
+			game.timer -= 1;
+
+			if (game.timer == 0) {
+				// next question and/or end game code goes here
+				$('#time-remaining').text(game.timer + ' Seconds');
+				clearInterval(counter);
+			}
+			else if (game.timer == 1) {
+				$('#time-remaining').text(game.timer + ' Second');
+			}
+			else {
+				$('#time-remaining').text(game.timer + ' Seconds');
+			}
+		}
 	}
 
-	Game.prototype.startGame = function() {
-		var div = $('<div>', {
-			'class': 'center-block',
-			'data-mode': this.mode.toLowerCase()
-		}).html(
-		'<p class="time-remaining-text">Time Remaining: <span id="time-remaining">' + 30 +
-		'</span> Seconds</p><br>' + 
-		'<p class="question-text">placeholder</p><br>' +
-		'<ul class="question-list">' +
-		'<li class="question">' + 'question 1 placeholder' + '</li>' +
-		'<li class="question">' + 'question 2 placeholder' + '</li>' +
-		'<li class="question">' + 'question 3 placeholder' + '</li>' +
-		'<li class="question">' + 'question 4 placeholder' + '</li>' +
-		'</ul>'
-		);
-/*
-		var p_time = $('<p>', {
-			'class': 'time-remaining-text'
-		}).text('Time Remaining: <span id="time-remaining">' + 30 + '</span> Seconds');
+	var questions = {
+		'html': [
 
-		var p_question = $('<p>', {
-			'class': 'question-text'
-		}).text('placeholder');
-*/
-		$('.main-game').append(div);
+		],
+
+		'css': [
+
+		],
+
+		'javascript': [
+
+		],
+
+		'jquery': [
+
+		]
 	}
 });
