bd3744d5f6cee6a5bde11d20419ddf23561e9e6f
morgansliman <morgansliman@gmail.com>
2016-11-01 18:51:47 -0400

    Just restructured some code and wrote more logic. Next commit will be comments outlining program flow so far.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
 	$('.button-mode').on('click', function() {
 		$('.mode-list').hide();
-		game.startGame($(this).text());
+		game.startGame($(this).text().toLowerCase());
 	});
 
 	var game = {
@@ -15,28 +15,54 @@ $(document).ready(function() {
 		numWrong: 0,
 		numTimeout: 0,
 		answerIndex: '',
+		json: false,
+		jsonQuestions: [],
+		jsonAnswers: [],
 
 		startGame: function(mode) {
+			if (this.json == false) {
+				this.getJSON(mode);
+			}
 			this.answerIndex = this.newQuestion(mode);
 			this.startCountdown();
 
 			$('.question').on('click', function() {
 				if ($(this).data('index') == game.answerIndex) {
+					clearInterval(counter);
 					game.numCorrect += 1;
 				} else {
+					clearInterval(counter);
 					game.numWrong += 1;
 				}
+				console.log('correct: ', game.numCorrect, '\n', 'wrong: ', game.numWrong);
+				game.answerIndex = game.newQuestion(mode);
+				game.startCountdown();
 			});
 		},
 
+		init: function() {
+			game.timer = 10;
+			$('.main-game').empty()
+
+		},
+
+		getJSON: function(mode) {
+			this.json = true;
+			this.jsonQuestions = jsonData[mode].questions;
+			this.jsonAnswers = jsonData[mode].answers;
+		},
+
 		newQuestion: function(mode) {
+			var index = Math.floor(Math.random() * game.jsonQuestions.length);
+			var correctAnswerIndex = game.jsonAnswers[index][4];
+
 			var div = $('<div>', {
 				'class': 'center-block game-wrapper',
-				'data-mode': mode.toLowerCase()
+				'data-mode': mode
 			}).html(
 			'<p class="time-remaining-text">Time Remaining: <span id="time-remaining">' + 
 			game.timer + ' Seconds</span></p>' + 
-			'<p class="question-text">' + jsonData[mode.toLowerCase()].question[0] + '</p><br>' +
+			'<p class="question-text">' + game.jsonQuestions[index] + '</p><br>' +
 			'<ul class="question-list center-block">' +
 			'<li class="question" data-index="0"></li>' +
 			'<li class="question" data-index="1"></li>' +
@@ -48,10 +74,12 @@ $(document).ready(function() {
 
 			var ul = $('.question-list').children();
 			for (var i = 0; i < 4; i++) {
-				ul.eq(i).text(jsonData[mode.toLowerCase()].answer[0][i]);
+				ul.eq(i).text(game.jsonAnswers[index][i]);
 			}
 
-			return jsonData[mode.toLowerCase()].answer[0][4];
+			game.jsonQuestions.splice(index, 1);
+			game.jsonAnswers.splice(index, 1);
+			return correctAnswerIndex;
 		},
 
 		startCountdown: function() {
